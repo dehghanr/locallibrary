2fd104b30b811f703439b9395f5d30b96d03fd97
dehghanr <dehghanr.mohammad@gmail.com>
2022-04-22 18:05:48 +0300

    init

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
